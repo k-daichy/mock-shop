33817e69b59945d87b5fd0a5f2de71183640a9e8
k-daichy <rusty.a.k.a.vympel@gmail.com>
2021-07-27 12:32:08 +0900

    remove material ui options

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,12 @@
 import 'tailwindcss/tailwind.css'
 import React from 'react';
 import Head from "next/head";
-import {ThemeProvider} from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import theme from '../src/theme';
 import {CartProvider} from 'use-shopping-cart'
 import getStripe from '../utils/get-stripejs'
 import * as config from '../config'
 import type {AppProps} from 'next/app'
 
 export default function MyApp({Component, pageProps}: AppProps) {
-  React.useEffect(() => {
-    // Remove the server-side injected CSS.
-    const jssStyles = document.querySelector('#jss-server-side');
-    if (jssStyles && jssStyles.parentElement) {
-      jssStyles.parentElement.removeChild(jssStyles);
-    }
-  }, []);
-
   return (
     <CartProvider
       mode="checkout-session"
@@ -29,11 +18,7 @@ export default function MyApp({Component, pageProps}: AppProps) {
           <title>My page</title>
           <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
         </Head>
-        <ThemeProvider theme={theme}>
-          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-          <CssBaseline/>
           <Component {...pageProps}/>
-        </ThemeProvider>
       </React.Fragment>
     </CartProvider>
   );
