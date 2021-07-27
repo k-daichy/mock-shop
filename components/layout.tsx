53080fa97ce2b0a790def3a3904e6142e9823487
k-daichy <rusty.a.k.a.vympel@gmail.com>
2021-07-27 21:31:03 +0900

    add link to top

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "next/dist/client/link";
 import ShoppingCart from "./ShoppingCart";
 
 interface LayoutProps {
@@ -10,10 +11,12 @@ export default function Layout({children}: LayoutProps) {
     <div>
       <header className="text-gray-100 bg-gray-900">
         <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+          <Link href="/">
           <a
             className="flex order-first lg:order-none lg:w-1/5 title-font font-medium items-center lg:items-center lg:justify-start mb-4 md:mb-0">
             <span className="ml-3 text-xl">Mock Shop</span>
           </a>
+              </Link>
           <nav className="flex lg:w-2/5 flex-wrap items-center text-base md:ml-auto">
           </nav>
           <div className="lg:w-2/5 inline-flex lg:justify-end ml-5 lg:ml-0">
