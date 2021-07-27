ae4c51b4e1609111bbb410e7b7fa271e11cb86fe
k-daichy <rusty.a.k.a.vympel@gmail.com>
2021-07-28 00:45:13 +0900

    fix margin

--- components/card/Card.tsx
+++ components/card/Card.tsx
@@ -3,7 +3,7 @@ import CardDescription from "./CardDescription"
 
 export default function Card({product}: any) {
   return (
-    <div className="p-5 m-5 rounded border-solid border-2">
+    <div className="p-5 m-auto rounded border-solid border-2">
       <div className="w-50 m-auto">
         <CardImage image={product.image}/>
         <CardDescription product={product}/>
